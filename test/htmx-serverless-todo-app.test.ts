fbf5524c72a21e568f655c0d2f85a5c3a8f6ad15
diego <smartcrashomg@gmail.com>
2024-01-13 22:17:15 +0100

    Initial commit

---
+++ test/htmx-serverless-todo-app.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HtmxServerlessTodoApp from '../lib/htmx-serverless-todo-app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/htmx-serverless-todo-app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HtmxServerlessTodoApp.HtmxServerlessTodoAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
