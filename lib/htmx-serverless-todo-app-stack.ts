f2f1591d895e9fd442cf0e0dd739636f160ae0a3
diego <smartcrashomg@gmail.com>
2024-01-14 11:01:34 +0100

    feat: create DynamoDB Todos table

--- lib/htmx-serverless-todo-app-stack.ts
+++ lib/htmx-serverless-todo-app-stack.ts
@@ -2,6 +2,7 @@ import { join } from 'node:path';
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
 import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
@@ -17,6 +18,23 @@ export class HtmxServerlessTodoAppStack extends cdk.Stack {
       ...defaultNodejsFunctionProps,
       entry: join(__dirname, 'lambdas', 'index.ts'),
     })
+
+    const table = new Table(this, 'Todos', {
+      partitionKey: {
+        name: 'id',
+        type: AttributeType.STRING,
+      },
+      tableName: 'todos',
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      /**
+       *  The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new table, and it will remain in your account until manually deleted. By setting the policy to
+       * DESTROY, cdk destroy will delete the table (even if it has data in it)
+       */
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // NOT recommended for production code
+    })
+
+    table.grantReadData(indexHandlerFunction)
 
     const api = new RestApi(this, 'Api', {})
 
