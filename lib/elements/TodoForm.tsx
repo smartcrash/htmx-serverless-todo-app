e7d4b436c2e7413ba9fa42589c622ffaa9d97ac0
diego <smartcrashomg@gmail.com>
2024-01-16 21:55:56 +0100

    feat: disallow dates in the past

--- lib/elements/TodoForm.tsx
+++ lib/elements/TodoForm.tsx
@@ -1,6 +1,8 @@
 import * as elements from "typed-html";
 
 export function TodoForm() {
+  const minDate = new Date().toISOString().split("T")[0];
+
   return (
     <form
       hx-post="/prod/todos"
@@ -11,7 +13,7 @@ export function TodoForm() {
     >
       <input minlength="3" name="title" placeholder="Title" required="true" />
       <textarea name="description" placeholder="Description"></textarea>
-      <input type="date" name="dueDate" />
+      <input min={minDate} type="date" name="dueDate" />
       <button type="submit">Submit</button>
     </form>
   )
