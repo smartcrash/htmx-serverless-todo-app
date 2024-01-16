ee63d04f5d65e1505d21b627f1223d745de10cf8
diego <smartcrashomg@gmail.com>
2024-01-16 20:10:36 +0100

    test: add <rootDir>/lib to roots

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/test'],
+  roots: ['<rootDir>/test', '<rootDir>/lib'],
   testMatch: ['**/*.test.ts'],
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
